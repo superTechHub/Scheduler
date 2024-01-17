88ae6e5f8f3dfe864b1ad982016593400195f847
Dev101034 <141519745+Dev101034@users.noreply.github.com>
2024-01-17 12:43:01 +0530

    Sechduler

---
+++ scheduler_Remove_hold.ps1
@@ -0,0 +1,13 @@
+﻿$instances = aws ec2 describe-instances --filters Name=tag:Scheduled,Values=schdeuled-22_to_9-hold,schdeuled-22_to_1-hold,schdeuled-22_to_9-hold --output json | ConvertFrom-Json
+
+foreach ($instance in $instances.Reservations.Instances) {
+    $instanceId = $instance.InstanceId
+    $instanceName = $instance.Tags.Where({ $_.Key -eq "Name" }).Value
+    $currentTagValue = $instance.Tags.Where({ $_.Key -eq "Scheduled" }).Value
+
+    if ($currentTagValue -in ("schdeuled-22_to_9-hold","schdeuled-22_to_1-hold","schdeuled-22_to_9-hold")) {
+        $newValue = $currentTagValue.Substring(0, $currentTagValue.Length - 5)  # Remove "-hold"
+        aws ec2 create-tags --resources $instanceId --tags Key=Scheduled,Value=$newValue
+        Write-Output "Instance $instanceName ($instanceId) has been updated with tag Scheduled: $newValue"
+    }
+}
---
+++ scheduler_hold.ps1
@@ -0,0 +1,20 @@
+﻿$instances = aws ec2 describe-instances --filters Name=tag:Scheduled,Values=schdeuled-22_to_9,schdeuled-22_to_1,schdeuled-22_to_9 --output json | ConvertFrom-Json
+
+foreach ($instance in $instances.Reservations.Instances) {
+    $instanceId = $instance.InstanceId
+    $instanceName = $instance.Tags.Where({ $_.Key -eq "Name" }).Value
+    $currentTagValue = $instance.Tags.Where({ $_.Key -eq "Scheduled" }).Value
+
+    if ($currentTagValue -eq "schdeuled-22_to_9") {
+        $newValue = "schdeuled-22_to_9-hold"
+    } elseif ($currentTagValue -eq "schdeuled-22_to_1") {
+        $newValue = "schdeuled-22_to_1-hold"
+    } else {
+        $newValue = "schdeuled-22_to_9-hold"  # Default value for other cases
+    }
+
+    if ($newValue -ne $currentTagValue) {  # Only update if the value has changed
+        aws ec2 create-tags --resources $instanceId --tags Key=Scheduled,Value=$newValue
+        Write-Output "Instance $instanceName ($instanceId) has been updated with tag Scheduled: $newValue"
+    }
+}
